048b37fa68d4a6fe65ec44af56b884ef58636e6b
sunnepazzy123 <sunnepazzy123@gmail.com>
2021-04-17 16:37:04 -0700

    first commit

---
+++ routes/home/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+router.all('/*', (req, res, next) => {
+    req.app.locals.layout = 'home';
+    next();
+  });
+  
+
+router.get("/", (req, res)=> res.render("home"))
+
+
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+dotenv.config()
+const path = require('path')
+
+//Middleware
+// app.use(morgan("dev"))
+
+//Use static asset file
+app.use(express.static(path.join(__dirname, 'public/')))
+app.use(express.static(path.join(__dirname, 'public/admin')))
+
+
+//SETUP TEMPLATE ENGINE
+app.engine("handlebars", exphbs({
+    defaultLayout: "home",
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+
+}))
+
+app.set('view engine', 'handlebars')
+
+//BodyParser
+app.use(express.json())
+
+//Loading Routes
+const homeRoute = require('./routes/home')
+
+//Use Routes
+app.use("/", homeRoute)
+
+
+
+
+const PORT = process.env.PORT || 2000
+
+app.listen(PORT, (req, res)=> console.log(`Server is running on port ${PORT}`))
